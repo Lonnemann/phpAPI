f206a8533767047d0536fe25675e9c3af478d8cd
Thomas Lonnemann <lonnemann@dma.do>
2015-10-21 06:25:38 +0200

    Added Foundation support, reloacated templates, added env support for easy custization

--- src/MVC.php
+++ src/MVC.php
@@ -9,7 +9,7 @@ class MVC extends Command
      *
      * @var string
      */
-    protected $signature = 'make:mvc {name}';
+    protected $signature = 'make:mvc {name} {--foundation} {--bootstrap}';
 
     /**
      * The console command description.
@@ -17,20 +17,21 @@ class MVC extends Command
      * @var string
      */
     protected $description = 'Creates a Model, a view, a migration and a controller and adds it to the routes.';
-
+    
     /**
-     * Generates files and their corresponing template
+     * Template Location definitions
      *
-     * @var string
+     * @var array ['output'=>'input']
      */
+
     protected $rewriteFiles =   [
                                     'app/::Name.php'                                =>  'vendor/phpapi/mvcgenerator/src/templates/model.php',
                                     'app/Http/Controllers/::NameController.php'     =>  'vendor/phpapi/mvcgenerator/src/templates/controller.php',
-                                    'resources/views/::name/create.blade.php'       =>  'vendor/phpapi/mvcgenerator/src/templates/create.blade.php',
-                                    'resources/views/::name/edit.blade.php'         =>  'vendor/phpapi/mvcgenerator/src/templates/edit.blade.php',
-                                    'resources/views/::name/index.blade.php'        =>  'vendor/phpapi/mvcgenerator/src/templates/index.blade.php',
-                                    'resources/views/::name/master.blade.php'       =>  'vendor/phpapi/mvcgenerator/src/templates/master.blade.php',
-                                    'resources/views/::name/show.blade.php'         =>  'vendor/phpapi/mvcgenerator/src/templates/show.blade.php'
+                                    'resources/views/::name/create.blade.php'       =>  'vendor/phpapi/mvcgenerator/src/templates/::engine/create.blade.php',
+                                    'resources/views/::name/edit.blade.php'         =>  'vendor/phpapi/mvcgenerator/src/templates/::engine/edit.blade.php',
+                                    'resources/views/::name/index.blade.php'        =>  'vendor/phpapi/mvcgenerator/src/templates/::engine/index.blade.php',
+                                    'resources/views/::name/master.blade.php'       =>  'vendor/phpapi/mvcgenerator/src/templates/::engine/master.blade.php',
+                                    'resources/views/::name/show.blade.php'         =>  'vendor/phpapi/mvcgenerator/src/templates/::engine/show.blade.php'
         
                                 ];
 
@@ -51,20 +52,27 @@ class MVC extends Command
      */
     public function handle()
     {
-        //Setting name 
+        // Template Engine Defintions
+        $this->templateEngine = env('TEMPLATE_ENGINE','bootstrap');
+        if ($this->option('foundation') !== false)
+            $this->templateEngine ='foundation';
+        if ($this->option('bootstrap') !== false)
+            $this->templateEngine ='bootstrap';
+
+        // Name Definition
         $name       =   strtolower($this->argument('name'));
-        if ($name===null)
+        if ($name === null)
             $name   =   strtolower($this->ask('Please define the name of the package'));
         
         //Create Model and Creation
-        $this->call('make:model',['name'=> ucfirst($name),'-m'=>true]);
+        $this->call('make:model',['name' => ucfirst($name),'-m' => true]);
         
         //Create a RESTfulController
-        $this->call('make:controller',['name'=>ucfirst($name).'Controller']);
+        $this->call('make:controller',['name' => ucfirst($name).'Controller']);
 
-       foreach ($this->rewriteFiles as $key => $value) {
-            $this->rewriteFile($name,$key,$value,'w');
-        } 
+        // write Templates
+        foreach ($this->rewriteFiles as $key => $value) 
+            $this->rewriteFile($name,$key,$value);
         
         //  Add a route to the Controller into the routes.php
         if (file_exists('app/Http/routes.php'))
@@ -73,28 +81,25 @@ class MVC extends Command
                 if (fwrite($handle,"\n"."Route::resource('/".strtolower($name)."','".ucfirst($name)."Controller');"))
                     if (fclose($handle))
                         $this->info("Added path to routes.php");
-        
-        /**/
-
     }
 
-    protected function rewriteFile($name,$file,$template,$type='w'){
-        $file=str_replace('::name',strtolower($name),$file);
-        $file=str_replace('::Name',ucfirst($name),$file);
-        $newContent=file_get_contents($template);  
-        $newContent=str_replace('::name',strtolower($name),$newContent);
-        $newContent=str_replace('::Name',ucfirst($name),$newContent);
+    protected function rewriteFile($name,$file,$template,$type = 'w')
+    {
+        $file = str_replace('::name',strtolower($name),$file);
+        $file = str_replace('::Name',ucfirst($name),$file);
+
+        $newContent = file_get_contents(str_replace("::engine", $this->templateEngine, $template));  
+        $newContent = str_replace('::name',strtolower($name),$newContent);
+        $newContent = str_replace('::Name',ucfirst($name),$newContent);
+
         if (strpos($file,'sources/views/'))
-        {
             if (!(file_exists("resources/views/$name")))
                 mkdir("resources/views/$name");
-            //$type="a+";
-        }
             
-        if ($handle=fopen($file,$type))
+        if ($handle = fopen($file,$type))
             if (fwrite($handle,$newContent))
                 if (fclose($handle))
                     $this->info("Added Content to ".$file);
+    }
 
-    }   
 }
